e70485b18ba820769612873b5f630887680210ca
LaChapeliere <emmamarion.barme@orange.fr>
2017-08-27 11:27:18 +0200

    Adding a dummy main file for testing

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author: LaChapeliere
+ * Copyright: GNU General Public License v3.0
+ * Created on: 27-08-2017
+ * Last modified on: 27-08-2017
+ */
+
+
+//main function for compilation tests
+
+#include <iostream>
+#include "simulEngine/Creature.h"
+using namespace std;
+
+int main(){
+
+    cout << "Hello World!" << endl;
+
+    return 0;
+
+}
