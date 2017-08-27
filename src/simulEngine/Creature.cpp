80bdb31be9d53e140fd3ad3ca7f5aca32fcf2ce1
LaChapeliere <emmamarion.barme@orange.fr>
2017-08-27 11:27:01 +0200

    Creation of empty Creature class

---
+++ src/simulEngine/Creature.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: LaChapeliere
+ * Copyright: GNU General Public License v3.0
+ * Created on: 27-08-2017
+ * Last modified on: 27-08-2017
+ */
+
+#include "Creature.h"
+
---
+++ src/simulEngine/Creature.h
@@ -0,0 +1,20 @@
+/*
+ * Author: LaChapeliere
+ * Copyright: GNU General Public License v3.0
+ * Created on: 27-08-2017
+ * Last modified on: 27-08-2017
+ */
+
+
+#ifndef CREATURE_H
+#define CREATURE_H
+
+//Class for the creatures
+class Creature
+{
+	public:
+    	
+	private:
+};
+
+#endif
