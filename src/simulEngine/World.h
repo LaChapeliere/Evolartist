725edb623116b00019f6c922a2dde8e381a59e47
LaChapeliere <emmamarion.barme@orange.fr>
2017-09-08 10:26:51 +0200

    World class: Adding attributes and constructor declaration

--- src/simulEngine/World.h
+++ src/simulEngine/World.h
@@ -14,10 +14,33 @@
 class World
 {
 private:
-	
+	/**
+	 * Size of the world grid
+	 */
+	const int m_size;
+
+	/**
+	 * Time since initialization, in simulation steps
+	 */
+	int m_age;
+
+	/**
+	 * Time since last manual modification, in simulation steps
+	 */
+	int m_incubationTime;
+
+	/**
+	 * Flattened matrix of Spot objects, represent the grid
+	 */
+	const std::vector<Spot> m_grid;
 
 public:
-	
+	/**
+ 	 * World constructor
+	 * @param size The size of the square grid
+ 	 * @return A new World object with the specified size of grid
+ 	 */
+	World(const int size);
     	
 };
 
