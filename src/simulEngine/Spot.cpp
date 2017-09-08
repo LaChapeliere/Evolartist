d67896330c0ee8ac68febb4c8d195f5959ba55ac
LaChapeliere <emmamarion.barme@orange.fr>
2017-09-08 15:21:03 +0200

    Create Spot class for the spots in the world grid

---
+++ src/simulEngine/Spot.cpp
@@ -0,0 +1,9 @@
+/**
+ * Author: LaChapeliere
+ * Copyright: GNU General Public License v3.0
+ * Created on: 08-09-2017
+ * Last modified on: 08-09-2017
+ */
+
+#include "Spot.h"
+
---
+++ src/simulEngine/Spot.h
@@ -0,0 +1,22 @@
+/**
+ * Author: LaChapeliere
+ * Copyright: GNU General Public License v3.0
+ * Created on: 08-09-2017
+ * Last modified on: 08-09-2017
+ */
+
+
+#ifndef SPOT_H
+#define SPOT_H
+
+//Class for the spots in the grid
+class Spot
+{
+private:
+	
+
+public:
+	    	
+};
+
+#endif
