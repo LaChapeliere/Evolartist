d91af284227f00da9bb2611a0890e71c97835ada
LaChapeliere <emmamarion.barme@orange.fr>
2017-09-08 15:28:24 +0200

    Spot class: attributes and constructor declaration

--- src/simulEngine/Spot.h
+++ src/simulEngine/Spot.h
@@ -9,14 +9,40 @@
 #ifndef SPOT_H
 #define SPOT_H
 
+#include <vector>
+
 //Class for the spots in the grid
 class Spot
 {
 private:
-	
+	/**
+	 * Horizontal coordinate of the position in the world grid
+	 */
+	const int m_x;
+
+	/**
+	 * Vertical coordinate of the position in the world grid
+	 */
+	const int m_y;
+
+	/**
+	 * Feeding power of the spot
+	 */
+	int m_food;
+
+	/**
+	 * List of Creatures on this spot
+	 */
+	std::vector<Creature> m_creatures;
 
 public:
-	    	
+	/**
+ 	 * Spot constructor
+ 	 * @param x Horizontal coordinate
+	 * @param y Vertical coordinate
+ 	 * @return A new Spot object with the specified location
+ 	 */
+	Spot(const int x, const int y);    	
 };
 
 #endif
