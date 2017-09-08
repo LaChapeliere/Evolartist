f2bd04ef6490bd4d9b3aeb9773cc869dda3e0806
LaChapeliere <emmamarion.barme@orange.fr>
2017-09-08 16:06:33 +0200

    Spot class: methods declarations

--- src/simulEngine/Spot.cpp
+++ src/simulEngine/Spot.cpp
@@ -7,3 +7,25 @@
 
 #include "Spot.h"
 
+Spot::Spot(const int x, const int y) {
+	m_x = x;
+	m_y = y;
+	m_food = rand() % 100;
+	m_creatures = std::vector<Creature const*>();
+}
+
+const int Spot::food() const {
+	return m_food;
+}
+
+const int nbCreatures() const {
+	return m_creatures.size();
+}
+
+Creature const* Spot::creatureFromIndex(const int id) const {
+	return m_creatures[id];
+}
+
+void Spot::growFood() {
+	m_food++;
+}
