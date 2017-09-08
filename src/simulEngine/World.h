370256e4d81b045fa27a2ceb83a495fa4b1ef86d
LaChapeliere <emmamarion.barme@orange.fr>
2017-09-08 10:18:43 +0200

    Creating World class

---
+++ src/simulEngine/World.h
@@ -0,0 +1,24 @@
+/**
+ * Author: LaChapeliere
+ * Copyright: GNU General Public License v3.0
+ * Created on: 08-09-2017
+ * Last modified on: 08-09-2017
+ */
+
+
+#ifndef WORLD_H
+#define WORLD_H
+
+
+//Class for the world grid
+class World
+{
+private:
+	
+
+public:
+	
+    	
+};
+
+#endif
