f3cebc55168af72a215815f056a4ef5209c35c34
LaChapeliere <emmamarion.barme@orange.fr>
2017-09-14 12:24:18 +0200

    World class: Adding method declarations for each step of the simulation

--- src/simulEngine/World.h
+++ src/simulEngine/World.h
@@ -10,6 +10,7 @@
 #define WORLD_H
 
 #include "Spot.h"
+#include "Creature.h"
 
 
 //Class for the world grid
@@ -36,6 +37,16 @@ private:
 	 */
 	const std::vector<Spot> m_grid;
 
+	/**
+	 * Identifier of most recent Creature object
+	 */
+	int m_lastCreatureId;
+
+	/**
+	 * List of Creature objects in the world
+	 */
+	std::vector<Creature> m_creatures;
+
 public:
 	/**
  	 * World constructor
@@ -43,7 +54,61 @@ public:
  	 * @return A new World object with the specified size of grid
  	 */
 	World(const int size);
-    	
+
+	
+	/**
+	 * Accessor of age of the world
+	 * @return m_age
+	 */
+	const int getAge() const;
+
+	/**
+	 * Accessor of incubation time of the world
+	 * @return m_incubationTime
+	 */
+	const int getIncubTime() const;
+
+
+	/**
+	 * Add a Creature to the world, in a random position
+	 */
+	void addCreature();
+
+	/**
+	 * Add a Creature to the world, in a specified position
+	 */
+	void addCreature(const int x, const int y);
+
+	/**
+	 * Delete a Creature from the world, according to its identifier
+	 */
+	void removeCreature(const int creatureId);
+
+	/**
+	 * Evaluate environment for each Creature object
+	 */
+	void evalEnvCreatures();
+
+	/**
+	 * Move all Creature objects
+	 */
+	void moveCreatures();
+
+	/**
+	 * Have Creatures interact in each Spot
+	 */
+	void interactBtwCreatures();
+	
+	/**
+	 * Have Creatures interact with their environment
+	 */
+	void interactCreaturesEnv();
+
+	/**
+	 * Survival and aging step
+	 */
+	void resolveTurn();
+
 };
 
 #endif
